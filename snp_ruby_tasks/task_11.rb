4051816279c646ecc8bd720c7b327c493d471a89
Kirilov.K.V <kvkirilv@ya.ru>
2019-02-26 13:53:29 +0000

    Task_N11 OK!

---
+++ snp_ruby_tasks/task_11.rb
@@ -0,0 +1,30 @@
+=begin
+Реализуйте класс Dessert c геттерами и сеттерами для полей класса name и
+calories, конструктором, принимающим на вход name и calories, а также двумя
+методами healthy? (возвращает true при условии калорийности десерта менее 200)
+и delicious? (возвращает true для всех десертов).	
+=end
+
+class Dessert
+	attr_accessor :name, :calories
+
+	def initialize(name, calories)
+        @name, @calories = name, calories
+    end
+
+    def healthy
+    	if @calories < 200
+    		return true 
+    	else
+    		return false 
+    	end    	
+    end 
+
+    def delicious
+    	return true 
+    end
+
+end
+
+dessert = Dessert.new('cake', 250)
+puts dessert.healthy
