c5a92b36474711d11abf0f81f03a0e71cc9bba5f
Kirilov.K.V <kvkirilv@ya.ru>
2019-03-03 18:12:50 +0000

    Task_N8 edit

--- snp_ruby_tasks/task_8.rb
+++ snp_ruby_tasks/task_8.rb
@@ -10,35 +10,19 @@ multiply_numbers(2.3) # => 6
 multiply_numbers([5, 6, 4]) # => 120
 =end
 
-def multiply_numbers(inputs)
-	if inputs == 0 
-		return nil
-	end
-	if inputs.instance_of?( Array )
-		multiply = 1
-		inputs.each {|item| multiply = multiply * item.to_i}
-		return multiply	
-	else
-		multiply = 1
-		inputs = inputs.to_s.split('')
-		inputs.each {|item| multiply = multiply * item.to_i}
-		return multiply	
-	end
+def multiply_numbers(inputs = nil)
+	return nil if inputs.nil?	
+	inputs = inputs.to_s.gsub(/[^0-9]/, '')
+	return nil if inputs.empty?
+	multy = 1
+	inputs.split("").each {|item|  multy = multy * item.to_i}
+	return multy
 end
 
-puts "Если вы собираетесь заполнять массив напишите 'array' иначе оставьте пустое поле"
-type_input = gets.chomp.downcase!
-if type_input == 'array'
-	inputs = []
-	puts "Пожалуйста заполните массив через ENTER:"
-	while true
-		input = gets.chomp
-		break if input.empty?
-		inputs << input
-	end
-else
-	puts "Пожалуйста заполните строку:"
-	inputs = gets.chomp
-	puts inputs.gsub!(/[^0-9]/, '').to_i
-end
-#puts multiply_numbers(inputs)
+# Test
+puts multiply_numbers() # => nil
+puts multiply_numbers('ss') # => nil
+puts multiply_numbers('1234') # => 24
+puts multiply_numbers('sssdd34') # => 12
+puts multiply_numbers(2.3) # => 6
+puts multiply_numbers([5, 6, 4]) # => 120
